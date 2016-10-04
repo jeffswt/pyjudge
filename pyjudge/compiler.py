867f3028142469bd9ee42b31976abfa0133aff2c
Geoffrey, Tang <jeffswt@outlook.com>
2016-10-04 17:04:39 +0800

    Added compiler wrapper for better orientation.

--- pyjudge/compiler.py
+++ pyjudge/compiler.py
@@ -1,3 +1,36 @@
+def wrap_compiler(input_class):
+    class CompilerWrapper(input_class):
+        def __init__(self, source_path):
+            ret = input_class.__init__(self, source_path)
+            self.__sequence = 0
+            return ret
+        def compile(self):
+            if self.__sequence != 0:
+                raise AttributeError('Source code already compiled')
+            ret = input_class.compile(self)
+            self.__sequence = 1
+            return ret
+        def execute(self):
+            if self.__sequence != 1:
+                raise AttributeError('Source code hadn\'t been compiled')
+            ret = input_class.execute(self)
+            self.__sequence = 2
+            return ret
+        def get_output(self):
+            if self.__sequence != 2:
+                raise AttributeError('No execution had been made')
+            ret = input_class.get_output(self)
+            return ret
+        def close(self):
+            if self.__sequence == 0:
+                raise AttributeError('Compiler handle already closed')
+            ret = input_class.close(self)
+            self.__sequence = 0
+            return ret
+        pass
+    return CompilerWrapper
+
+@wrap_compiler
 class Compiler:
     """ Basic compiler, all functions defined here when invoked without further
     definition would raise a NotImplementedError. Use inheritance for this
