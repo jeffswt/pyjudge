27f6204662ae32741c66b2d86da1126038d47f67
Geoffrey, Tang <jeffswt@outlook.com>
2016-10-04 21:40:51 +0800

    Changed to self-defined arguments.

--- pyjudge/config.py
+++ pyjudge/config.py
@@ -1,11 +1,26 @@
 
+"""
+# About configuring compiler arguments
+
+The following arguments may be passed into the compilers or intepreters, if such
+events are supported by the compilers and intepreters. Otherwise they would be
+implemented by the external methods of the module.
+
+    source_file : The filename of the code.
+    output_file : The output executable of the code.
+    min_memory : Minimum required memory of the heap space.
+    max_memory : Maximum allowed memory of the heap space.
+
+Others would be implemented by this module.
+"""
+
 __configs = {
-    'gcc_path': 'C:/Program Files (x86)/Dev-Cpp/MinGW64/bin/gcc.exe',
-    'g++_path': 'C:/Program Files (x86)/Dev-Cpp/MinGW64/bin/g++.exe',
-    'fpc_path': None,
-    'python2_path': 'C:/Programs/Python2/python2.exe',
-    'python3_path': 'C:/Programs/Python3/python3.exe',
-    'javac_path': None,
+    'gcc_args': ['C:/Program Files (x86)/Dev-Cpp/MinGW64/bin/gcc.exe', '-O0', '-g0', '-o', '${output_file}', '${source_file}"'],
+    'g++_args': ['C:/Program Files (x86)/Dev-Cpp/MinGW64/bin/g++.exe', '-O0', '-g0', '-o', '${output_file}', '${source_file}"'],
+    'fpc_args': None,
+    'python2_args': ['C:/Programs/Python2/python2.exe', '${source_file}'],
+    'python3_args': ['C:/Programs/Python3/python3.exe', '${source_file}'],
+    'javac_args': None,
 }
 
 def get_config(idx):
