dd61f037c5af892acf9c9673ed45bbd643c85206
Geoffrey, Tang <jeffswt@outlook.com>
2016-10-04 22:20:12 +0800

    Added temporary files management.

---
+++ pyjudge/tmpmgmt.py
@@ -0,0 +1,29 @@
+
+import os
+import uuid
+
+from . import config
+
+__tmp_files = set()
+
+def create_tmpfile():
+    tmp_dir = config.get_config('tmp_dir')
+    if not os.path.exists(tmp_dir):
+        os.makedirs(tmp_dir)
+    while True:
+        uid = str(uuid.uuid4())
+        pth = os.path.join(tmp_dir, uid)
+        if not os.path.exists(pth):
+            break
+        continue
+    return pth
+
+def remove_tmpfile(fil):
+    tmp_dir = config.get_config('tmp_dir')
+    fn = os.path.basename(fil)
+    pth = os.path.join(tmp_dir, fn)
+    if os.path.exists(pth):
+        os.remove(pth)
+    if fn in __tmp_files:
+        __tmp_files.remove(fn)
+    return
