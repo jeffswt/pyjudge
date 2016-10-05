7b6c23c3a1b4c3fb8b9a58e31cf6d8ea644418e4
Geoffrey, Tang <jeffswt@outlook.com>
2016-10-05 13:03:36 +0800

    Now able to join wrapped lines.

--- pyjudge/table.py
+++ pyjudge/table.py
@@ -22,6 +22,24 @@ class Table:
         if len(res) <= 0:
             res.append(' ' * width)
         return res
+    @classmethod
+    def join_wrap_lines(self, left, right, lwidth=10, rwidth=10, lalign='left', ralign='right'):
+        if type(left) == str:
+            left = self.wrap_lines(left, lwidth, align=lalign)
+        if type(right) == str:
+            right = self.wrap_lines(right, rwidth, align=ralign)
+        # Done wrapping to lists, syncing height
+        lwidth = len(left[0]) # Guranteed correctness
+        rwidth = len(right[0])
+        while len(right) < len(left):
+            right.append(' ' * rwidth)
+        while len(left) < len(right):
+            left.append(' ' * lwidth)
+        # Combining
+        res = []
+        for i in range(0, len(left)):
+            res.append(left[i] + right[i])
+        return res
     def __init__(self, title, data):
         self.title = title
         self.data = data
