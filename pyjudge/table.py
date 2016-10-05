3a4445f833440ea41dcb6fcd4e3cb2f80dbcefb5
Geoffrey, Tang <jeffswt@outlook.com>
2016-10-05 13:03:47 +0800

    Created representative output of tables.

--- pyjudge/table.py
+++ pyjudge/table.py
@@ -40,7 +40,27 @@ class Table:
         for i in range(0, len(left)):
             res.append(left[i] + right[i])
         return res
+    @classmethod
+    def insert_in_lines(self, lines, pos, inject):
+        ret_l = []
+        for line in lines:
+            ret_l.append(line[:pos] + inject + line[pos:])
+        return ret_l
     def __init__(self, title, data):
         self.title = title
         self.data = data
         return
+    def __repr__(self):
+        console_width = 80
+        row_widths = [int(console_width * 0.25) - 3, console_width - int(console_width * 0.25) + 3]
+        out_l = []
+        out_l += self.join_wrap_lines(' ', self.title, lwidth=1, rwidth=console_width-1, ralign='left')
+        out_l.append('=' * row_widths[0] + '=+=' + '=' * row_widths[1])
+        for row in self.data:
+            tmp_l = self.join_wrap_lines(row[0], row[1], lwidth=row_widths[0], rwidth=row_widths[1], lalign='right', ralign='left')
+            out_l += self.insert_in_lines(tmp_l[:1], row_widths[0], ' T ') + self.insert_in_lines(tmp_l[1:], row_widths[0], ' | ')
+        fmt_s = ''
+        for line in out_l:
+            fmt_s += line + '\n'
+        return fmt_s
+    pass
