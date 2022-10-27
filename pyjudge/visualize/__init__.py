3cfd220b9ffcde32b8c3929ce494badf258823e4
d0j1a_1701 <dajia1701@gmail.com>
2022-10-27 22:51:32 +0800

    remove line numbers

--- pyjudge/visualize/__init__.py
+++ pyjudge/visualize/__init__.py
@@ -82,33 +82,9 @@ html_template_data_main = r"""
 s_stdout = test['execution-status']['output']['stdout']
 s_userout = test['execution-status']['user-code']['stdout']
 s_stdin = test['execution-status']['input']['stdout']
-l_sout = s_stdout.split('\n')
-l_uout = s_userout.split('\n')
-l_sin = s_stdin.split('\n')
-n_out = []
-n_sout = []
-n_sin = []
-def make_line_count(i, ps_arr=None):
-    maxlen = len(str(max(len(l_sout), len(l_uout))))
-    if ps_arr:
-        maxlen = len(str(len(ps_arr)))
-    s = str(i).rjust(maxlen, ' ')
-    return s
-for i in range(0, len(l_uout)):
-    if i >= len(l_sout):
-        n_out.append(make_line_count(i) + ' | ' + l_uout[i])
-        continue
-    n_sout.append(make_line_count(i) + ' | ' + l_sout[i])
-    if l_uout[i] == l_sout[i]:
-        n_out.append(make_line_count(i) + ' | ' + l_uout[i])
-    else:
-        n_out.append(make_line_count(i) + '###' + l_uout[i])
-    pass
-for i in range(0, len(l_sin)):
-    n_sin.append(make_line_count(i, ps_arr=l_sin) + ' | ' + l_sin[i])
-test['execution-status']['input']['stdout'] = '\n'.join(n_sin)
-test['execution-status']['user-code']['stdout'] = '\n'.join(n_out)
-test['execution-status']['output']['stdout'] = '\n'.join(n_sout)
+test['execution-status']['input']['stdout'] = s_stdout
+test['execution-status']['user-code']['stdout'] = s_userout
+test['execution-status']['output']['stdout'] = l_sin
 %>
                         <div class="col-sm-4">
                             <h4><b>Input</b></h4>
@@ -132,15 +108,17 @@ test['execution-status']['output']['stdout'] = '\n'.join(n_sout)
 </div>
 """
 
+
 def create(json_data):
     """ Create visualized HTML with JSON input in dict(). """
     data = html_template_data_main
     if type(data) == bytes:
         data = data.decode('utf-8', 'ignore')
     data = mako.template.Template(
-        text = data,
-        input_encoding = 'utf-8').render(
-            json_data = json_data
-        )
-    data = vis_html_data.html_template_data_begin + data + vis_html_data.html_template_data_end
+        text=data,
+        input_encoding='utf-8').render(
+            json_data=json_data
+    )
+    data = vis_html_data.html_template_data_begin + \
+        data + vis_html_data.html_template_data_end
     return data
